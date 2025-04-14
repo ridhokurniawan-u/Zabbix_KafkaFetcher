279e13444f6d1371fa4a748894183df4b2a85959
Ridho Kurniawan <109494665+ridhokurniawan-u@users.noreply.github.com>
2025-04-14 17:35:17 +0700

    [FIX] Wrong consumer offset reading

--- kafka_fetcher.py
+++ kafka_fetcher.py
@@ -4,21 +4,22 @@ import time
 import subprocess
 from kafka import KafkaConsumer, KafkaAdminClient, TopicPartition
 
-psk_file = "/loc/of/encrypt.psk"
-psk_identity = "psk"
+psk_file = "/muh/frickin/encrypt.psk"
+psk_identity = "nuh uh"
 
-group_id = "my-cool-group"
+group_id = "muh-kul-group"
 topic = "test-topic"
-broker = "192.168.1.1:9092"
+broker = "muh broker ip:muh port"
 
 admin = KafkaAdminClient(bootstrap_servers=broker)
 consumer = KafkaConsumer(group_id=group_id, bootstrap_servers=broker)
+
 
 prev_offsets = {}
 prev_time = time.time()
 
 logging.basicConfig(
-    filename="C:/Users/devop/Documents/kafkascript/kafka.log",
+    filename="/home/tester/kafka.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
@@ -38,15 +39,19 @@ while True:
     tps = [TopicPartition(topic, p) for p in partitions]
     consumer.assign(tps)
 
+    # Kafka Consumer Lag
     total_lag = 0
 
+    # Kafka Message Troughput
     current_time = time.time()
     elapsed_time = current_time - prev_time
 
+    end_offsets = consumer.end_offsets(tps)
+
     message_throughput = 0
-    
+
     for tp in tps:
-        latest = consumer.position(tp)
+        latest = end_offsets[tp]
         committed = consumer.committed(tp)
         total_lag += latest - (committed or 0)
         prev = prev_offsets.get(tp.partition, latest)
@@ -58,13 +63,13 @@ while True:
     through_per_sec = message_throughput / elapsed_time if elapsed_time else 0
 
     print("Current Consumer Lag:", total_lag)
-    time.sleep(25)
+    time.sleep(5)
 
     message_throughput = 0
 
-    zabbix_host = "192.168.1.249"  
+    zabbix_host = "duh"
     zabbix_item_key = f'kafka.lag[{group_id},{topic}]'
-    zabbix_hostname = "Tester Client" 
+    zabbix_hostname = "fit to dash"
 
     cmd_lag = [
         "zabbix_sender",
@@ -82,9 +87,9 @@ while True:
         "--tls-connect=psk",
         "--tls-psk-identity", psk_identity,
         "--tls-psk-file", psk_file,
-        "-z", zabbix_host, 
+        "-z", zabbix_host,
         "-s", zabbix_hostname,
-        "-k", f"kafka.topic.throughput[{topic}]", 
+        "-k", f"kafka.topic.throughput[{topic}]",
         "-o", str(through_per_sec)
     ]
 
@@ -93,20 +98,20 @@ while True:
         "--tls-connect=psk",
         "--tls-psk-identity", psk_identity,
         "--tls-psk-file", psk_file,
-        "-z", zabbix_host, 
+        "-z", zabbix_host,
         "-s", zabbix_hostname,
-        "-k", f"kafka.consumer.heartbeat[{group_id}]", 
+        "-k", f"kafka.consumer.heartbeat[{group_id}]",
         "-o", "1"
     ]
 
     cmd_partition = [
-        "zabbix_sender", 
+        "zabbix_sender",
         "--tls-connect=psk",
         "--tls-psk-identity", psk_identity,
         "--tls-psk-file", psk_file,
-        "-z", zabbix_host, 
+        "-z", zabbix_host,
         "-s", zabbix_hostname,
-        "-k", f"kafka.topic.partitions[{topic}]", 
+        "-k", f"kafka.topic.partitions[{topic}]",
         "-o", str(len(partitions))
     ]
 
@@ -117,13 +122,13 @@ while True:
         broker_status = 0
 
     cmd_health = [
-        "zabbix_sender", 
+        "zabbix_sender",
         "--tls-connect=psk",
         "--tls-psk-identity", psk_identity,
         "--tls-psk-file", psk_file,
-        "-z", zabbix_host, 
+        "-z", zabbix_host,
         "-s", zabbix_hostname,
-        "-k", f"kafka.broker.status[{broker}]", 
+        "-k", f"kafka.broker.status[{broker}]",
         "-o", str(broker_status)
     ]
 
